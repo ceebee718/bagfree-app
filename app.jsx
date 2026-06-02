a937b8caccd0c928701f0d75c8d4060916fa9a9d
Larry Cumberbatch <cbatch7@gmail.com>
2026-06-02 14:28:28 -0400

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1408,7 +1408,7 @@ function VideoSection() {
       </div>
       <div className="video-frame">
         <iframe
-          src="https://player.vimeo.com/video/1195690381?badge=0&autopause=0&player_id=0&app_id=58479"
+          src="https://player.vimeo.com/video/1195690381?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           title="BagFree"
